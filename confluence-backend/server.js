a2804c889d7e74bf3e56df3ba5a2637d4dc163c7
shivaguruv <shivaguruvenkateswaran@gmail.com>
2025-03-04 14:48:30 +0530

    Update server.js

--- confluence-backend/server.js
+++ confluence-backend/server.js
@@ -20,7 +20,7 @@ const AUTH_TYPE = 'Basic'; // Use OAuth for production
 
 // Login Endpoint
 app.post('/api/login', async (req, res) => {
-  console.log('resquest'+req.body);
+  console.log('resquest'+json.stringify(req.body));
     const { username, password } = req.body;
 
     if (!username || !password) {
